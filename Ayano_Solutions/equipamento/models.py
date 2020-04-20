fdf4f149cb2c3438d12bc3e78b60237173f4c018
lukkas-lax <31673865+lukkas-lax@users.noreply.github.com>
2020-04-20 18:34:53 -0300

    Update models.py

--- Ayano_Solutions/equipamento/models.py
+++ Ayano_Solutions/equipamento/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 from django.db import models  
 class Equipamento(models.Model):  
     eid = models.CharField(max_length=20)  
-    ename = models.CharField(max_length=100)  
-    emanutencao = models.CharField(max_length=3)  
-    edataC = models.DateField() 
+    ename = models.CharField(max_length=100)   
+    edataC = models.DateField()
+    estatus = models.CharField(max_length=20)
+    efornecedor = models.CharField(max_length=100)
+    earea = models.CharField(max_length=50)
+    eqtd = models.PositiveIntegerField()
     class Meta:  
         db_table = "equipamento"
